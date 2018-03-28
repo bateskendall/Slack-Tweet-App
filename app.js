d2c8c3a41bdd35da681d5f16f68e48e5357cf35a
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-03-28 03:59:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 // app.js
-var Twitter = require('twitter');
 var config = require('./config.js');
-var T = new Twitter(config);
 var prompt = require('prompt');
+var Twitter = require('twitter');
+var T = new Twitter(config);
 
-var newTweet = { status: 'sample tweet' }
+// get input from user
+prompt.start();
+prompt.get(['tweet'], function(err, result) {
+
+	// store user input
+	var newTweet = { status: result.tweet };
 	
-T.post('statuses/update', newTweet, function(err, data, response) {
-	// if unsuccessful, log error message
-	if(err){
-		console.log(err[0].message);
-	}
-	else{
-		console.log('Tweet successfully posted!');
-	}
+	// post tweet
+	T.post('statuses/update', newTweet, function(err, data, response) {
+		// if unsuccessful, log error message
+		if(err){ console.log(err[0].message); }
+		// else display success message
+		else{ console.log('Tweet successfully posted!'); }
+	});
 });
