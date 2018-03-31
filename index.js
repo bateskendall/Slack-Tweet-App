bf440f18cac75d95ed9804d416a945ac11d00d8c
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-03-31 03:28:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+// app.js
+require('dotenv').config();
+
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+const qs = require('querystring');
+const tweet = require('./tweet');
+const debug = require('debug')('slash-command-template:index');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('<h2>TweetBot running.</p>');
+});
+
+/*
+ * Endpoint to receive /tweet slash command from Slack.
+ * Checks verification token and opens a dialog to capture more info.
+ */
+app.post('/commands', (req, res) => {
+	// extract the verification token, slash command text,
+	// and trigger ID from payload
+	const { token, text, trigger_id } = req.body;
+	
+	// check that the verification token matches expected value
+	if (token === process.env.SLACK_VERIFICATION_TOKEN) {
+		// create the dialog payload - includes the dialog structure, Slack API token,
+		// and trigger ID
+		const dialog = {
+			token: process.env.SLACK_ACCESS_TOKEN,
+			trigger_id,
+			dialog: JSON.stringify({
+				title: 'Send a tweet!',
+				callback_id: 'submit-tweet',
+				submit_label: 'Submit',
+				elements: [{
+					label: 'Tweet',
+					type: 'text',
+					name: 'title',
+					value: text,
+					hint: 'What\'s happening?',
+				}],
+			})
+		};
+		
+		// Open dialog
+		axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
+		.then((result) => {
+			debug('dialog.open: %o', result.data);
+			res.send('');
+		}).catch((err) => {
+			debug('dialog.open call failed: %o', err);
+			res.sendStatus(500);
+			});
+	} else {
+		debug('Verification token mismatch');
+		res.sendStatus(500);
+	};
+});
+
+/*
+ * Endpoint to receive the dialog submission. Checks the verification token
+ * and creates a Helpdesk ticket
+ */
+app.post('/interactive-component', (req, res) => {
+	const body = JSON.parse(req.body.payload);
+	
+	// check that the verification token matches expected value
+	if (body.token === process.env.SLACK_VERIFICATION_TOKEN) {
+		debug(`Form submission received: ${body.submission.trigger_id}`);
+		
+		// immediately respond with a empty 200 response to let
+		// Slack know the command was received
+		res.send('');
+		
+		// create tweet
+		tweet.create(body.user.id, body.submission);
+	} else {
+		debug('Token mismatch');
+		res.sendStatus(500);
+	};
+});
+
+app.listen(process.env.PORT, () => {
+	console.log('App listening on port ' + process.env.PORT);
+});
---
+++ tweet.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const debug = require('debug')('slash-command-template:tweet');
+const qs = require('querystring');
+const config = require('./config');
+const users = require('./users');
+const Twitter = require('twitter');
+
+var T = new Twitter(config);
+
+//Send tweet confirmation.
+const sendConfirmation = (tweet) => {
+	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+		token: process.env.SLACK_ACCESS_TOKEN,
+		channel: tweet.userId,
+		text: 'Tweet sent!',
+		attachments: JSON.stringify([{
+			title: `${tweet.userId} sent a tweet!`,
+			text: tweet.text,
+			fields: [{
+				title: 'Tweet',
+				value: tweet.title,
+			}],
+		}]),
+	})).then((result) => {
+		debug('sendConfirmation: %o', result.data);
+		}).catch((err) => {
+			debug('sendConfirmation error: %o', err);
+			console.error(err);
+		});
+};
+
+// Create tweet.
+const create = (userId, submission) => {
+	
+	const tweet = {status: submission.title};
+	
+	// post tweet
+	T.post('statuses/update', tweet, function(err, data, response) {
+		// if unsuccessful, log error message
+		if(err){ console.log(err[0].message); }
+		// else display success message
+		else{
+			sendConfirmation(tweet);
+			return tweet;
+		},
+	});
+}
+
+module.exports = { create, sendConfirmation };
