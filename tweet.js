3d2ec6cc9c78af6df3eacdf313054d50c8ae053a
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-01 17:55:28 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -19,7 +19,7 @@ const sendConfirmation = (tweet) => {
 			title: `${tweet.userId} sent a tweet!`,
 			text: tweet.text,
 			fields: [{
-				title: tweet,
+				title: tweet.status,
 				value: tweet.title,
 			}],
 		}]),
