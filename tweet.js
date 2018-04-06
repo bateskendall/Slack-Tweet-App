477f5af59859e4858af87f36a18b0fa11b7b6665
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-06 01:52:41 -0400

    Test (#5)
    * Update tweet.js
    
    * Delete users.js
    
    * Update tweet.js
    
    * Add files via upload

--- tweet.js
+++ tweet.js
@@ -1,23 +1,9 @@
 const config = require('./config');
-const users = require('./users');
 const Twitter = require('twitter');
 
 var T = new Twitter(config);
 
 // Create tweet.
-const create = (userId, submission) => {
-	
-	var newTweet = {status: submission.title};
-	
-	// post tweet
-	T.post('statuses/update', newTweet, function(err, data, response) {
-		// if unsuccessful, log error message
-		if(err){ console.log(err[0].message); }
-		// else display success message
-		else{
-			return newTweet;
-		};
-	});
-}
+const create = (userId, submission) => { T.post('statuses/update', {status: submission.title}); }
 
 module.exports = { create };
