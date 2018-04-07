db6a31ecb4f1abe65534d79896d1b9667e458a72
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-07 00:13:49 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -3,7 +3,23 @@ const Twitter = require('twitter');
 
 var T = new Twitter(config);
 
+//Send tweet confirmation.
+const sendConfirmation = (newTweet) => {
+	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+		token: process.env.SLACK_ACCESS_TOKEN,
+		channel: users.find(userId),
+		text: 'Tweet sent!',
+		attachments: JSON.stringify([{
+			title: `${users.info.name} sent a tweet!`,
+			text: newTweet.status,
+		}]),
+	}))
+};
+
 // Create tweet.
-const create = (userId, submission) => { T.post('statuses/update', {status: submission.title}); }
+const create = (userId, submission) => { 
+  T.post('statuses/update', {status: submission.title});
+  sendConfirmation(newTweet);
+}
 
 module.exports = { create };
