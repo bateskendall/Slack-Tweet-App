efd876bc8c213e0af618e346e3c93571357c383d
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-09 20:06:15 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -3,8 +3,8 @@ const Twitter = require('twitter');
 
 var T = new Twitter(config);
 
-//Send tweet confirmation.
-const sendConfirmation = (newTweet) => {
+//Confirm to user that tweet was sent
+const confirmTweet = (newTweet) => {
 	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
 		token: process.env.SLACK_ACCESS_TOKEN,
 		channel: users.find(userId),
@@ -22,4 +22,4 @@ const create = (userId, submission) => {
   sendConfirmation(newTweet);
 }
 
-module.exports = { create };
+module.exports = { create, confirmTweet };
