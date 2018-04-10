375009fa3b68e6f8501b784ad22d1e49767248b8
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-09 21:29:46 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -4,6 +4,6 @@ const Twitter = require('twitter');
 var T = new Twitter(config);
 
 // Create tweet.
-const create = (userId, submission) => { T.post('statuses/update', {status: submission.title}) }
+const create = (userId, submission) => { T.post('statuses/update', {status: submission.title}); }
 
 module.exports = { create };
