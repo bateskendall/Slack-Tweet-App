b4345553e472cf4f59f0993664e7297fcf753f92
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-09 21:07:53 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -6,10 +6,7 @@ var T = new Twitter(config);
 // Create tweet.
 const create = (userId, submission) => { 
   if ( submission.title.length > 280 ) {
-    	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
-        token: process.env.SLACK_ACCESS_TOKEN,
-        channel: userId,
-        text: 'Your tweet is too long. Please keep your tweets under 280 characters.'
+
   } else {
     T.post('statuses/update', {status: submission.title});
   };
