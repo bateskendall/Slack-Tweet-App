d81f631067f88bd814142805a0f19d27e9cc46e2
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-18 23:40:50 -0400

    Send confirmation (#7)
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Delete tweet.js
    
    * Add files via upload
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Update tweet.js
    
    * Delete tweet.js
    
    * Add files via upload
    
    * Delete index.js
    
    * Add files via upload
    
    * Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,9 +1,28 @@
+const axios = require('axios');
 const config = require('./config');
+const dotenv = require('dotenv');
+const qs = require('querystring');
 const Twitter = require('twitter');
 
 var T = new Twitter(config);
 
-// Create tweet.
-const create = (userId, submission) => { T.post('statuses/update', {status: submission.title}); }
+// Confirm tweet to user.
+function confirmTweet(user, name, id) {
+	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+		token: process.env.SLACK_ACCESS_TOKEN,
+		channel: user,
+		text: 'Tweet sent! \n https://twitter.com/' + name + '/status/' + id
+  }));
+};
 
-module.exports = { create };
+// Create tweet.
+const create = (userId, submission) => { 
+	T.post('statuses/update', {status: submission.title}, function (err, data, response) {
+		// Get username and id of newly created tweet to include in url to confirm to user.
+		var username = data.user.screen_name;
+		var tweetId = data.id_str;
+		confirmTweet(userId, username, tweetId);
+  });
+}
+
+module.exports = { confirmTweet, create };
