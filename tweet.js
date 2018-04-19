d1bf8a137b60b697f97d114c247d3a189191a4a6
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-18 22:36:07 -0400

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -12,8 +12,7 @@ function confirmTweet(user, name, id) {
 	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
 	token: process.env.SLACK_ACCESS_TOKEN,
 	channel: user,
-	text: 'Tweet sent!\
-	https://twitter.com/' + name + '/status/' + id,
+	text: 'Tweet sent!\nhttps://twitter.com/' + name + '/status/' + id,
 	//attachments: JSON.stringify([
 	//	{
 	//		text: 'https://twitter.com/' + name + '/status/' + id,
