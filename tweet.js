8188cc75249145f94a94a66cda7f19d8c2e0d21c
bateskendall <37778076+bateskendall@users.noreply.github.com>
2018-04-18 20:21:06 -0400

    Add files via upload

---
+++ tweet.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const config = require('./config');
+const dotenv = require('dotenv');
+const qs = require('querystring');
+const Twitter = require('twitter');
+
+var T = new Twitter(config);
+
+// Confirm tweet to user.
+function confirmTweet(id, newTweet) {
+	axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+	token: process.env.SLACK_ACCESS_TOKEN,
+	channel: id,
+	text: 'Tweet sent!',
+	attachments: JSON.stringify([{
+		title: `${id} sent a tweet!`
+	}]);
+  }));
+};
+
+// Create tweet.
+const create = (userId, submission) => { 
+  T.post('statuses/update', {status: submission.title});
+  confirmTweet(userId, submission.title);
+}
+
+module.exports = { create };
